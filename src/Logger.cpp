50a86ed2a0aa40bc6ffb2ff31fd6fe76207e20d7
woodcap <woodcap@outlook.com>
2025-11-21 17:42:20 +0800

    set log level

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -2,8 +2,21 @@
 #include <iostream>
 #include <ctime>
 
+// Current minimum log level
+static LogLevel current_log_level = DEFAULT_LOG_LEVEL;
+
+// Set the minimum log level to display
+void set_log_level(LogLevel level) {
+  current_log_level = level;
+}
+
 // Simple logger function
 void log_message(LogLevel level, const std::string& message) {
+  // Only log if the message level is at or above the current log level
+  if (level > current_log_level) {
+    return;
+  }
+  
   const char* levelStr[] = {"[ERROR]", "[WARN]", "[INFO]", "[DEBUG]"};
   time_t now = time(nullptr);
   char timeStr[20];
--- src/Logger.h
+++ src/Logger.h
@@ -11,7 +11,19 @@ enum LogLevel {
   LOG_DEBUG
 };
 
+// Set default log level based on build type
+#ifdef NDEBUG
+  // Release build: only show errors
+  #define DEFAULT_LOG_LEVEL LOG_ERROR
+#else
+  // Debug build: show info and above
+  #define DEFAULT_LOG_LEVEL LOG_INFO
+#endif
+
 // Simple logger function
 void log_message(LogLevel level, const std::string& message);
+
+// Set the minimum log level to display
+void set_log_level(LogLevel level);
 
 #endif // LOGGER_H
